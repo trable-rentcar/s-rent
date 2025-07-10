7492c16cf5d81f790d186e36512326c40751aff6
trable-rentcar <yhyeon9991@naver.com>
2025-07-10 14:36:02 +0900

    Revert "."
    This reverts commit 26e995e7ecdc74a8eaab94a3887caa7478744cd2.

--- js/contents.js
+++ js/contents.js
@@ -9,16 +9,4 @@ $(document).ready(function(){
         $('.cts_car .car_list > ul > li').removeClass('active')
         $(this).addClass('active')
    })
-
-   const params = new URLSearchParams(window.location.search);
-   const className = params.get("class");
-
-   if (className) {
-      // 해당 클래스 가진 첫 번째 버튼 자동 클릭
-      const targetButton = document.querySelector(`.${className}`);
-      if (targetButton) {
-         targetButton.click();
-         targetButton.classList.add('clicked-button'); // 스타일 강조 (선택)
-      }
-   }
 })
